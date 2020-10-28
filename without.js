d92cb7bd4a5ac2cfe1e4724d3075c92d129e3f5c
Alberto Olvera Sotres <mty2yyz@gmail.com>
2020-10-28 00:12:03 +0000

    added new file without.js

---
+++ without.js
@@ -0,0 +1,46 @@
+const eqArrays = function(arr1, arr2) {
+  // let output = false;
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const assertArraysEqual = function(arr1, arr2) {
+  if (eqArrays(arr1, arr2)) {
+    console.log(`✅✅✅Assertion Passed: ${arr1} === ${arr2}`);
+  } else {
+    console.log(`🛑🛑🛑Assertion Failed: ${arr1} !== ${arr2}`);
+  }
+};
+
+const without = function(source, itemsToRemove) {
+  const newArray = source.slice();
+  for (i = 0; i < newArray.length; i++) {
+    for (j = 0; j < itemsToRemove.length; j++) {
+      if (newArray[i] === itemsToRemove[j]) {
+        newArray.splice(i, 1);
+        i--; // adjust index for removed item.
+      }
+    }
+  }
+  return newArray;
+}
+
+assertArraysEqual(without([1, 2, 3], [1]), [2, 3]);
+assertArraysEqual(without(["1", "2", "3"], [1, 2, "3"]), ["1", "2"]);
+assertArraysEqual(without([true, false, true, false], [true]), [false, false]);
+assertArraysEqual(without(["a", "b", "c"], []), ["a", "b", "c"]);
+assertArraysEqual(without([], ["a", "b", "c"]), []);
+
+const words = ["hello", "world", "lighthouse"];
+without(words, ["lighthouse"]);
+// Make sure the original array was not altered by the without function
+assertArraysEqual(words, ["hello", "world", "lighthouse"]);
+
+// 7:09 --> 7:26; 7:57 --> 8:xx
