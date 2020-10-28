1493cd9b696f10f985910fe9700553944bbf2830
Alberto Olvera Sotres <mty2yyz@gmail.com>
2020-10-28 19:45:40 +0000

    updating countOnly.js after running eslint

--- countOnly.js
+++ countOnly.js
@@ -12,7 +12,7 @@ const countOnly = function(allItems, itemsToCount) {
   const results = {};
   for (const item of allItems) {
     if (itemsToCount[item]) {
-      if(results[item]) {
+      if (results[item]) {
         results[item]++;
       } else {
         results[item] = 1;
