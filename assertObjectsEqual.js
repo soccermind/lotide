6e0726bb97a1e71478e63044f74bccb53a77e2b4
Alberto Olvera Sotres <mty2yyz@gmail.com>
2020-10-29 00:50:23 +0000

    Adding new file assertObjectsEqual.js

---
+++ assertObjectsEqual.js
@@ -0,0 +1,60 @@
+const eqArrays = function(arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+// Returns true if both objects have identical keys with identical values.
+// Otherwise you get back a big fat false!
+const eqObjects = function(object1, object2) {
+  if (Object.keys(object1).length !== Object.keys(object2).length) {
+    return false;
+  } else {
+    for (const key in object1) {
+      if (!object2[key]) {
+        // key from object1 does not exist in object2
+        return false;
+      } else if (typeof object1[key] !== typeof object2[key]) {
+        // values of the same key are of different type
+        return false;
+      } else if (Array.isArray(object1[key]) && !eqArrays(object1[key], object2[key])) {
+        // values of the same key are different arrays
+        return false;
+      } else if (!Array.isArray(object1[key]) && object1[key] !== object2[key]) {
+        // values of the same key are not array and do not match
+        return false;
+      }
+    }
+  }
+  return true;
+};
+
+const assertObjectsEqual = function(actual, expected) {
+  const inspect = require('util').inspect;
+  if (eqObjects(actual, expected)) {
+    console.log(`✅✅✅Assertion Passed: ${inspect(actual)} === ${inspect(expected)}`);
+  } else {
+    console.log(`🛑🛑🛑Assertion Failed: ${inspect(actual)} !== ${inspect(expected)}`);
+  }
+};
+
+// TEST CODE:
+const ab = { a: "1", b: 2 };
+const ba = { b: 2, a: "1" };
+assertObjectsEqual(ab, ba);
+
+const abc = { a: "1", b: "2", c: "3" };
+assertObjectsEqual(ab, abc);
+
+const cd = { c: "1", d: ["2", 3] };
+const dc = { d: ["2", 3], c: "1" };
+assertObjectsEqual(cd, dc);
+
+const cd2 = { c: "1", d: ["2", 3, 4] };
+assertObjectsEqual(cd, cd2);
