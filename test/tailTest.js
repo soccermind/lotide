5a9e0a6c56e8556c66e0590aae07876620134e53
Alberto Olvera Sotres <mty2yyz@gmail.com>
2020-11-02 19:14:17 +0000

    Refactoring tail.js, updating head.js, adding new test file tailTest.js

---
+++ test/tailTest.js
@@ -0,0 +1,23 @@
+const tail = require('../tail');
+const assertEqual = require('../assertEqual');
+
+// Test case 1: Check the returned array elements
+const result = tail(["Hello", "Lighthouse", "Labs"]);
+assertEqual(result.length, 2);
+assertEqual(result[0], "Lighthouse");
+assertEqual(result[1], "Labs");
+
+// Test case 2: Confirm that original array is not modified
+const words = ["Yo Yo", "Lighthouse", "Labs"];
+tail(words);
+assertEqual(words.length, 3);
+
+// Test case 3: an array with one element should yield and empty array
+const one = ["Hello"];
+const zero = tail(one);
+assertEqual(zero.length, 0);
+
+// Test case 4: and empty array should yield and empty array
+const none = [];
+const empty = tail(none);
+assertEqual(none.length, empty.length);
